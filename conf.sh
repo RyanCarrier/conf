d10fadc63383e9c63deead9df2bd3ea7b18aa81d
rcarrier <rcarrier@byteback.com.au>
2016-05-07 19:23:50 +1000

    kinda tested #closenough

--- conf.sh
+++ conf.sh
@@ -9,17 +9,18 @@ do
   fi
   echo -n "Checking $f..."
   current="$PWD/$f"
-  homef="~/$f"
+  rl=$(readlink -f ~/$f)
   if [ -f ~/$f ]; then
-    if [ "$homef" -ef "$current" ];then
-      echo "OK!"
+    if [ $rl == $current ]; then
+      echo -e "\tOK!"
       continue
     else
       echo -ne "\nBacking up $f..."
-      mv "~/$f" "~/$f.backup"
+      mv ~/$f ~/$f.backup
     fi
   fi
   echo -ne "\nLinking $f..."
-  ln -s "$current" "~/$f"
-  echo "OK!"
+  ln -s $current ~/$f
+  echo -e "\tOK!"
 done
+echo "Configs copied!"
