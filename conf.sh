53e745217f5320120f9edfeda04e1a0942f2606c
rcarrier <rcarrier@byteback.com.au>
2016-06-27 15:12:54 +1000

    updated some git stuff

--- conf.sh
+++ conf.sh
@@ -24,3 +24,13 @@ do
   echo -e "\tOK!"
 done
 echo "Configs copied!"
+
+echo "Adding gitstat"
+if [ "$(id -u)" != "0" ]; then
+  echo "Need sudo for copying gitstat python script;"
+  echo "sudo ln -s ./gitstat /usr/local/bin/gitstat"
+  sudo ln -s ./gitstat /usr/local/bin/gitstat
+else
+  ln -s ./gitstat /usr/local/bin/gitstat
+fi
+echo "gitstat added (I think)"
