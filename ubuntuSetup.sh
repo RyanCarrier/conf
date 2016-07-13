8a1695ef3523999192759ab69aeafb21d39d6bc8
rcarrier <rcarrier@byteback.com.au>
2016-07-14 01:04:31 +1000

    updated for yapf and python checkingwq ;

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -9,7 +9,10 @@ add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
 
 apt-get update
 
-apt-get install -y curl ubuntu-restricted-extras tar tmux thefuck vim rsync openssh-server traceroute qbittorrent vlc htop zip unzip
+apt-get install -y curl ubuntu-restricted-extras tar tmux thefuck vim rsync openssh-server traceroute qbittorrent vlc htop zip unzip python-pip
+
+pip install pep8
+pip install autopep8
 
 #Chrome
 curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -28,7 +31,7 @@ dpkg -i atom*.deb
 apt-get install -fy
 rm atom*.deb
 	#Atom Packages
-apm install autocomplete-go builder-go file-icons git-plus go-config go-get go-plus go-rename gofmt gometalinter-linter gorename linter navigator-godev tester-go
+apm install autocomplete-go builder-go file-icons git-plus go-config go-get go-plus go-rename gofmt gometalinter-linter gorename linter navigator-godev tester-go atom-beautify python-yapf autocomplete-python
 	#Atom Themes
 apm install atom-material-syntax atom-material-syntax-dark atom-material-syntax-light atom-material-ui genesis-syntax genesis-ui one-dark-vivid-syntax
 
