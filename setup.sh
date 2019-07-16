9df47877b0f19c5f5102ee723c51f75e6849bf41
Ryan Carrier <ryan.j.carrier@gmail.com>
2019-07-16 11:09:56 +1000

    fix

--- setup.sh
+++ setup.sh
@@ -45,19 +45,18 @@ echo "getting oh my zsh"
 
 sh -c "$(curl -fsSl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed '/\s*env\s\s*zsh\s*/d')"
 
-cd "$HOME" || echo "fail to cd home $HOME" && exit
 echo "getting fonts"
 git clone https://github.com/powerline/fonts.git /tmp/
-cd /tmp/fonts || echo "no fonts folder" && exit
+cd /tmp/fonts || {echo "no fonts folder" ; exit}
 
 sudo ./install.sh
 
 echo "applying rcarrier patch to zsh"
-cd ~/.oh-my-zsh/ || echo "no .oh-my-zsh folder" && exit
+cd ~/.oh-my-zsh/ || {echo "no .oh-my-zsh folder"; exit}
 cp ~/conf/.oh-my-zsh.patch ./
 git apply ./.oh-my-zsh.patch
 rm ./.oh-my-zsh.patch
-cd ~/conf || echo "fail to cd to conf" && exit
+cd ~/conf || {echo "fail to cd to conf" ; exit}
 
 echo "applying confs"
 if [ "$CONFS" = true ];then
