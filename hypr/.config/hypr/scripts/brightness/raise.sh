b5d3c7231c93413105c2e2c6fed5ba635892c6cd
Ryan Carrier <ryan.j.carrier@gmail.com>
2023-07-31 12:24:55 +1000

    add hypr, waybar

---
+++ hypr/.config/hypr/scripts/brightness/raise.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+L=$(light -G|cut -d'.' -f1)
+VALUE=10
+if [ "$L" -lt 101 ] && [ "$L" -gt 90 ]; then
+	VALUE=5
+elif [ "$L" -lt 91 ] && [ "$L" -gt 10 ]; then
+	VALUE=10
+elif [ "$L" -lt 11 ] && [ "$L" -gt 5 ]; then
+	VALUE=5
+else
+	VALUE=1
+fi
+light -A "$VALUE"
