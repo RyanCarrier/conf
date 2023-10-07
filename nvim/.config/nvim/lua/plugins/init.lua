cee1bef045aa844d9c5b39d007ec333dc4745117
Ryan Carrier <ryan.j.carrier@gmail.com>
2023-10-07 23:36:25 +1000

    update for indent line v3 cbf making it better

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -188,12 +188,20 @@ return {
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    main = 'ibl',
+
+    config = function()
+      -- local highlight = { "CursorColumn" }
+      -- local highlight = { "CursorColumn", "Whitespace" }
+      require("ibl").setup({
+        -- indent = { highlight = highlight, char = "" },
+        -- whitespace = {
+        --   highlight = highlight,
+        --   remove_blankline_trail = false,
+        -- },
+        scope = { enabled = false }
+      })
+    end
   },
 
   -- "gc" to comment visual regions/lines
