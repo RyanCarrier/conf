a05aea32ee2fd7a0e6fc135eb5dc45a95d8023e5
Ryan Carrier <ryan.j.carrier@gmail.com>
2024-02-12 22:50:11 +1000

    idk if this will work but we will find out

--- nvim/.config/nvim/lua/plugins/chatgpt.lua
+++ nvim/.config/nvim/lua/plugins/chatgpt.lua
@@ -3,9 +3,16 @@ return {
     event = "VeryLazy",
     lazy = true,
     config = function()
+        local apifile = "/home/rcarrier/.chatgpt.api.key"
+        local f = io.open(apifile, "r")
+        local file_exists = f ~= nil and io.close(f)
+        if not file_exists then
+            print("No ChatGPT API key found")
+            return
+        end
         local chatgpt = require("chatgpt")
         chatgpt.setup({
-            api_key_cmd = "cat /home/rcarrier/.chatgpt.api.key",
+            api_key_cmd = "cat " .. apifile,
             -- chat = {
             --     keymaps = {
             --         submit = { "<C-m>", "<C-Enter>", "<C-t>" },
