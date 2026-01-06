1f980449c4731833e16aa8bf3196ae286f4d4229
Ryan Carrier <ryan.j.carrier@gmail.com>
2026-01-06 21:38:38 +1000

    only source if exist

--- all/.rcarrier_profile.sh
+++ all/.rcarrier_profile.sh
@@ -42,7 +42,9 @@ export PATH=$PATH:"$JAVA_HOME/bin"
 export PATH="$PATH:/home/rcarrier/.shorebird/bin"
 export PATH=/home/rcarrier/.opencode/bin:$PATH
 export PATH="$PATH:/home/rcarrier/.turso"
-. "$HOME/.cargo/env"
+if [ -f "$HOME/.cargo/env" ]; then
+	. "$HOME/.cargo/env"
+fi
 # set PATH so it includes user's private bin if it exists
 if [ -d "$HOME/bin" ]; then
 	PATH=$PATH:"$HOME/bin"
